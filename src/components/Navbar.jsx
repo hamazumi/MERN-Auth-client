58b5f3cc08a38f03a5c724231f01fbd4b8a42f21
hamazumi <khamamoto12@gmail.com>
2021-07-08 14:52:51 -0700

    register form added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,23 @@
 import { Link } from 'react-router-dom'
 
 export default function Navbar(props) {
-    return (
-        <nav>  
-            <Link to="/">
-                Home
-            </Link>
 
-        {/* Only sees if user is logged in */}
-
+    //if user is logged in 
+    const loggedIn = (
+        <>
             <Link to="/profile">
-                Profile
+                    Profile
             </Link>
 
             <Link to="/">
                 <span onClick={props.handleLogout}>Logout!</span>
             </Link>
+        </>
+    )
 
-            {/* If user is logged out */}
-
+    //if user is logged out
+    const loggedOut = (
+        <>
             <Link to="/login">
                 Login
             </Link>
@@ -26,6 +25,16 @@ export default function Navbar(props) {
             <Link to="/register">
                 Register
             </Link>
-        </nav>
+        </>
+    )
+    return (
+        <nav>  
+            <Link to="/">
+                Home
+            </Link>
+
+            {props.currentUser ? loggedIn : loggedOut}
+
+       </nav>
     )
 }
