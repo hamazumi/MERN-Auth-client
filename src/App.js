e366009a413419b1965277412798e2315a685144
hamazumi <khamamoto12@gmail.com>
2021-07-08 10:15:38 -0700

    Routes added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar.jsx'
+import Login from './components/Login.jsx'
+import Register from './components/Register.jsx'
+import Profile from './components/Profile.jsx'
+import Welcome from './components/Welcome.jsx'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Router>
+      <header>
+        <Navbar />
       </header>
-    </div>
+      <div className="App">
+        <Switch>
+          
+          <Route 
+            exact path="/"
+            component={Welcome}
+          />
+
+          <Route 
+            path="/register"
+            render={ props => <Register {...props} /> }
+          />
+
+          <Route 
+            path="/login"
+            render={ props => <Login {...props} /> }
+          />
+
+          <Route 
+            path="/profile"
+            render={ props => <Profile {...props} /> }
+          />
+
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
